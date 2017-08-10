39f6b590497fa86d91535785de2253cef7ba1589
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-10 19:33:50 +0200

    Add HGNC integration functions

---
+++ src/bio2bel_hgnc/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Exports HGNC Equivalences"""
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel_hgnc'
+__description__ = "A package for converting HGNC to BEL"
+__url__ = 'https://github.com/bio2bel/hgnc'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'
