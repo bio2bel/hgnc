9ec965b5d4e434a1392a603902e79db3379e8320
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-05 14:55:04 +0100

    Add orthodoxy and equivalence enrichment tests

--- src/bio2bel_hgnc/enrich.py
+++ src/bio2bel_hgnc/enrich.py
@@ -8,9 +8,10 @@ from pybel.constants import NAMESPACE
 def get_node(graph, node, manager=None):
     """Gets a node from the PyHGNC database, whether it has a HGNC, RGD, MGI, or EG identifier.
 
-    :param pybel.BELGraph graph:
+    :param pybel.BELGraph graph: A BEL graph
     :param tuple node: A PyBEL node tuple
-    :rtype: pyhgnc.models.HGNC
+    :param optional[pyhgnc.QueryManager] manager: A PyHGNC database manager
+    :rtype: pyhgnc.manager.models.HGNC
     """
     raise NotImplementedError
 
@@ -18,8 +19,43 @@ def get_node(graph, node, manager=None):
 def add_metadata(graph, node, manager=None):
     """Add to the node based on PyHGNC's database
 
-    :param pybel.BELGraph graph:
+    :param pybel.BELGraph graph: A BEL Graph
     :param tuple node: A PyBEL node tuple
     :param optional[pyhgnc.QueryManager] manager: A PyHGNC database manager
     """
     raise NotImplementedError
+
+
+def add_node_equivalencies(grpah, node, manager=None, add_leaves=False):
+    """Given an HGNC node, add all equivalencies to EG and UniProt
+
+    :param pybel.BELGraph graph: A BEL graph
+    :param tuple node: A PyBEL node tuple
+    :param optional[pyhgnc.QueryManager] manager: A PyHGNC database manager
+    :param bool add_leaves: Should equivalencies that are not already in the graph be added?
+    """
+    raise NotImplementedError
+
+
+def add_node_orthologies(graph, node, manager=None, add_leaves=False):
+    """Given a node that's HGNC, add orthology relationships
+
+    :param pybel.BELGraph graph: A BEL graph
+    :param tuple node: A PyBEL node tuple
+    :param optional[pyhgnc.QueryManager] manager: A PyHGNC database manager
+    :param bool add_leaves: Should orthologs that are not already in the graph be added?
+    """
+    raise NotImplementedError
+
+
+def add_orthologies(graph, manager=None, add_leaves=False):
+    """Adds orthology relationships for all HGNC nodes to all of the available data for nodes that are in the
+    graph
+
+    :param pybel.BELGraph graph: A BEL graph
+    :param optional[pyhgnc.QueryManager] manager: A PyHGNC database manager
+    :param bool add_leaves: Should orthologs that are not already in the graph be added?
+    """
+    for node, data in graph.nodes(data=True):
+        if NAMESPACE in data and data[NAMESPACE] == 'HGNC':
+            add_node_orthologies(graph, node, manager=manager, add_leaves=add_leaves)
--- tests/test_enrich_metadata.py
+++ tests/test_enrich_metadata.py
@@ -6,15 +6,19 @@ import tempfile
 import unittest
 
 import pyhgnc
-from bio2bel_hgnc.enrich import add_metadata, get_node
+from bio2bel_hgnc.enrich import add_metadata, add_node_equivalencies, add_node_orthologies, get_node
 from pybel import BELGraph
+from pybel.constants import EQUIVALENT_TO, ORTHOLOGOUS, RELATION, unqualified_edge_code
 from pybel.dsl.nodes import protein
 from pybel.examples import sialic_acid_graph
 from pybel.examples.sialic_acid_example import cd33
+from pybel.parser.canonicalize import po_to_tuple
 from pyhgnc import QueryManager
 from tests.constants import hcop_test_path, hgnc_test_path
 
 log = logging.getLogger(__name__)
+
+equivalence_code = unqualified_edge_code[EQUIVALENT_TO]
 
 
 class TestEnrich(unittest.TestCase):
@@ -137,3 +141,91 @@ class TestEnrich(unittest.TestCase):
 
         self.assertEqual('CD33 molecule', sialic_acid_graph.get_node_label(cd33_tuple))
         self.assertEqual('1659', sialic_acid_graph.get_node_identifier(cd33_tuple))
+
+    def test_add_equivalency(self):
+        graph = BELGraph()
+        cd33_hgnc_tuple = graph.add_node_from_data(protein(name='CD33', namespace='HGNC'))
+        cd33_eg_tuple = graph.add_node_from_data(protein(identifier='945', namespace='EG'))
+
+        self.assertEqual(2, graph.number_of_nodes())
+        self.assertEqual(0, graph.number_of_edges())
+
+        add_node_equivalencies(graph, cd33_hgnc_tuple, manager=self.manager, add_leaves=False)
+
+        self.assertEqual(2, graph.number_of_nodes())
+        self.assertEqual(2, graph.number_of_edges())
+
+        self.assertIn(cd33_eg_tuple, graph.edge[cd33_hgnc_tuple])
+        v = list(graph.edge[cd33_hgnc_tuple][cd33_eg_tuple].values())[0]
+        self.assertIn(RELATION, v)
+        self.assertEqual(EQUIVALENT_TO, v[RELATION])
+
+        self.assertIn(cd33_hgnc_tuple, graph.edge[cd33_eg_tuple])
+        v = list(graph.edge[cd33_eg_tuple][cd33_hgnc_tuple].values())[0]
+        self.assertIn(RELATION, v)
+        self.assertEqual(EQUIVALENT_TO, v[RELATION])
+
+    def test_add_equivalency_with_leaves(self):
+        graph = BELGraph()
+        cd33_hgnc_tuple = graph.add_node_from_data(protein(name='CD33', namespace='HGNC'))
+
+        self.assertEqual(1, graph.number_of_nodes())
+
+        add_node_equivalencies(graph, cd33_hgnc_tuple, manager=self.manager, add_leaves=False)
+
+        cd33_ed = protein(identifier='945', namespace='EG')
+
+        self.assertTrue(graph.has_node_with_data(cd33_ed))
+
+        cd33_eg_node = po_to_tuple(cd33_ed)
+
+        self.assertIn(cd33_eg_node, graph.edge[cd33_hgnc_tuple])
+        self.assertIn(equivalence_code, graph.edge[cd33_hgnc_tuple][cd33_eg_node])
+
+    def test_add_orthology(self):
+        """Tests adding orthologies when both nodes are identified by name"""
+        graph = BELGraph()
+        cd33_hgnc_tuple = graph.add_node_from_data(protein(name='CD33', namespace='HGNC'))
+        cd33_mgi_tuple = graph.add_node_from_data(protein(name='Cd33', namespace='MGI'))
+
+        self.assertEqual(2, graph.number_of_nodes())
+        self.assertEqual(0, graph.number_of_edges())
+
+        add_node_orthologies(graph, cd33_hgnc_tuple, manager=self.manager, add_leaves=False)
+
+        self.assertEqual(2, graph.number_of_nodes())
+        self.assertEqual(2, graph.number_of_edges())
+
+        self.assertIn(cd33_mgi_tuple, graph.edge[cd33_hgnc_tuple])
+        v = list(graph.edge[cd33_hgnc_tuple][cd33_mgi_tuple].values())[0]
+        self.assertIn(RELATION, v)
+        self.assertEqual(ORTHOLOGOUS, v[RELATION])
+
+        self.assertIn(cd33_hgnc_tuple, graph.edge[cd33_mgi_tuple])
+        v = list(graph.edge[cd33_mgi_tuple][cd33_hgnc_tuple].values())[0]
+        self.assertIn(RELATION, v)
+        self.assertEqual(ORTHOLOGOUS, v[RELATION])
+
+    def test_add_orthology_target_has_id(self):
+        """Tests adding orthologies when the target is identified by its database identiier"""
+        graph = BELGraph()
+        cd33_hgnc_tuple = graph.add_node_from_data(protein(name='CD33', namespace='HGNC'))
+        cd33_mgi_id_tuple = graph.add_node_from_data(protein(identifier='99440', namespace='MGI'))
+
+        self.assertEqual(2, graph.number_of_nodes())
+        self.assertEqual(0, graph.number_of_edges())
+
+        add_node_orthologies(graph, cd33_hgnc_tuple, manager=self.manager, add_leaves=False)
+
+        self.assertEqual(2, graph.number_of_nodes())
+        self.assertEqual(2, graph.number_of_edges())
+
+        self.assertIn(cd33_hgnc_tuple, graph.edge[cd33_mgi_id_tuple])
+        v = list(graph.edge[cd33_mgi_id_tuple][cd33_hgnc_tuple].values())[0]
+        self.assertIn(RELATION, v)
+        self.assertEqual(ORTHOLOGOUS, v[RELATION])
+
+        self.assertIn(cd33_mgi_id_tuple, graph.edge[cd33_hgnc_tuple])
+        v = list(graph.edge[cd33_hgnc_tuple][cd33_mgi_id_tuple].values())[0]
+        self.assertIn(RELATION, v)
+        self.assertEqual(ORTHOLOGOUS, v[RELATION])
