ee0e2933346d1e95d5f5cac1d49acd9d36f256b7
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-12 13:18:59 +0100

    Wrap PyHGNC populate command

--- src/bio2bel_hgnc/cli.py
+++ src/bio2bel_hgnc/cli.py
@@ -2,10 +2,20 @@
 
 import click
 
+import pyhgnc
+from .constants import DEFAULT_CACHE_CONNECTION
+
 
 @click.group()
 def main():
     """Bio2BEL HGNC Utilities"""
+
+
+@main.command()
+@click.option('-c', '--connection', help="Defaults to {}".format(DEFAULT_CACHE_CONNECTION))
+def populate(connection):
+    """Populate the database"""
+    pyhgnc.update(connection=connection)
 
 
 if __name__ == '__main__':
--- src/bio2bel_hgnc/constants.py
+++ src/bio2bel_hgnc/constants.py
@@ -2,6 +2,18 @@
 
 """Enrichment functions for BEL graphs"""
 
+from os import environ, makedirs, path
+
+MODULE_NAME = 'hgnc'
+#: The default directory where PyBEL files, including logs and the  default cache, are stored. Created if not exists.
+BIO2BEL_DIR = environ.get('BIO2BEL_DIRECTORY', path.join(path.expanduser('~'), '.pybel', 'bio2bel'))
+DATA_DIR = path.join(BIO2BEL_DIR, MODULE_NAME)
+makedirs(DATA_DIR, exist_ok=True)
+
+DEFAULT_CACHE_NAME = '{}.db'.format(MODULE_NAME)
+DEFAULT_CACHE_LOCATION = path.join(DATA_DIR, DEFAULT_CACHE_NAME)
+DEFAULT_CACHE_CONNECTION = environ.get('BIO2BEL_CONNECTION', 'sqlite:///' + DEFAULT_CACHE_LOCATION)
+
 #: Encodings from https://www.genenames.org/cgi-bin/statistics
 encodings = {
     # protein-coding gene
