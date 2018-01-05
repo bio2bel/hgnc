b1664f6d7bec821d7d3dd8b533291285585fd8e1
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-01-05 20:27:25 +0100

    Update constant setting

--- src/bio2bel_hgnc/constants.py
+++ src/bio2bel_hgnc/constants.py
@@ -2,17 +2,12 @@
 
 """Enrichment functions for BEL graphs"""
 
-from os import environ, makedirs, path
+from bio2bel.utils import get_connection, get_data_dir
 
 MODULE_NAME = 'hgnc'
 #: The default directory where PyBEL files, including logs and the  default cache, are stored. Created if not exists.
-BIO2BEL_DIR = environ.get('BIO2BEL_DIRECTORY', path.join(path.expanduser('~'), '.pybel', 'bio2bel'))
-DATA_DIR = path.join(BIO2BEL_DIR, MODULE_NAME)
-makedirs(DATA_DIR, exist_ok=True)
-
-DEFAULT_CACHE_NAME = '{}.db'.format(MODULE_NAME)
-DEFAULT_CACHE_LOCATION = path.join(DATA_DIR, DEFAULT_CACHE_NAME)
-DEFAULT_CACHE_CONNECTION = environ.get('BIO2BEL_CONNECTION', 'sqlite:///' + DEFAULT_CACHE_LOCATION)
+DATA_DIR = get_data_dir(MODULE_NAME)
+DEFAULT_CACHE_CONNECTION = get_connection(MODULE_NAME)
 
 #: Encodings from https://www.genenames.org/cgi-bin/statistics
 encodings = {
