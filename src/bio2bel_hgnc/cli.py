0224ee2ba0afdc17f8e942d061fda20336db9e6e
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-27 10:54:21 +0200

    Remove commands from CLI

--- src/bio2bel_hgnc/cli.py
+++ src/bio2bel_hgnc/cli.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import sys
+"""Command line interface for Bio2BEL HGNC."""
 
-import click
 import logging
 
 from .manager import Manager
@@ -10,23 +9,6 @@ from .manager import Manager
 log = logging.getLogger(__name__)
 
 main = Manager.get_cli()
-
-
-@main.command()
-@click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
-@click.pass_obj
-def write_gene_belns(manager, output):
-    """Write gene namespace"""
-    manager.write_gene_bel_namespace(output)
-
-
-@main.command()
-@click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
-@click.pass_obj
-def write_gene_family_belns(manager, output):
-    """Write gene family namespace"""
-    manager.write_gene_family_bel_namespace(output)
-
 
 if __name__ == '__main__':
     main()
