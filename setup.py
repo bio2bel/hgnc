3b21621aa21001728dd3eceee3100ee8dbecba68
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-11 11:18:50 +0200

    Add python37 to setup
    [skip ci]

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering :: Bio-Informatics'
 ]
 INSTALL_REQUIRES = [
