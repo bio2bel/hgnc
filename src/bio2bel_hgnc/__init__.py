46e186e46765e2c93738b440fe779898f5acc259
ddomingof <danieldomingofernandez@hotmail.com>
2019-01-14 11:01:14 +0100

    Bump version: 0.2.2 → 0.2.3-dev

--- src/bio2bel_hgnc/__init__.py
+++ src/bio2bel_hgnc/__init__.py
@@ -6,7 +6,7 @@ from .gfam_manager import Manager as FamilyManager  # noqa: F401
 from .manager import Manager  # noqa: F401
 from .utils import get_version  # noqa: F401
 
-__version__ = '0.2.2'
+__version__ = '0.2.3-dev'
 
 __title__ = 'bio2bel_hgnc'
 __description__ = "A package for converting HGNC to BEL"
--- src/bio2bel_hgnc/constants.py
+++ src/bio2bel_hgnc/constants.py
@@ -4,7 +4,7 @@
 
 from bio2bel import get_data_dir
 
-VERSION = '0.2.2'
+VERSION = '0.2.3-dev'
 
 MODULE_NAME = 'hgnc'
 #: The default directory where PyBEL files, including logs and the  default cache, are stored. Created if not exists.
